544eea1afba7cc70bd34eadf2cf31e3dfe3e5f88
Torbjørn Marø <torbjorn.maro@gmail.com>
2016-02-21 08:02:32 +0100

    REQUEST, not quite as intended yet

---
+++ examples/4_request_response.lisp
@@ -0,0 +1,23 @@
+
+(defvar *client* (nats:make-connection :name "*client*"))
+(defvar *server* (nats:make-connection :name "*server*"))
+
+(nats:wait-for-connection *client*)
+(nats:wait-for-connection *server*)
+
+(setf nats:*debug* t)
+
+(nats:subscribe *server* "echo"
+  (lambda (msg &optional reply-to)
+    (format t "SERVER: got '~A'~%" msg)
+    (when reply-to
+      (nats:publish *server* reply-to msg))))
+
+
+;; Request a reply
+(nats:request *client* "echo" "I'd like a reply"
+  (lambda (msg &optional reply-to)
+    (format t "CLIENT: got '~A' '~A'~%" msg reply-to)))
+
+;; Fire and forget..
+(nats:publish *client* "echo" "I don't really care")
